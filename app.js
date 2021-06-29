c09b8ba1a0aaea38d8083473b8b2d081c064bf13
mahmood114 <mahmoodalmohri@gmail.com>
2021-06-29 18:45:05 +0300

    Finished deleting feature

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
 const express = require("express");
-const data = require("./data.js");
+let books = require("./data.js");
 const cors = require("cors");
 
 const app = express();
 
-// app.use(cors())
+app.use(cors())
 
-app.get("/", (req, res) => {
-    res.json(data)
+app.get("/books", (req, res) => {
+    res.json(books)
 });
 
-app.listen(8000, () => {
-    console.log("hoy")
-});
+app.delete("/books/:bookId", (req, res) => {
+    const {bookId} = req.params;
+    const foundBook = books.find(book => book.id === +bookId);
+    
+    if (foundBook) {
+        books = books.filter(book => book.id !== +bookId);
+        res.json({
+            message: `Book with ID ${bookId} was deleted successfully`
+        })
+        res.status(204).end();
+
+        console.log(books);
+    }
+    else {
+        res.status(404);
+        res.json({
+            message: "No book with this ID was found!"
+        })
+
+    }
+    
+
+})
+
+app.listen(8000);
